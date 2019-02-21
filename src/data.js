3938167e18fe88684504bce2c7f39b6dec368d81
ItzelMB <it.mobec@gmail.com>
2019-02-20 21:06:19 -0600

    Se muestran cards en interfaz con datos de objeto

--- src/data.js
+++ src/data.js
@@ -1,21 +1,4 @@
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
+const showPokemon = POKEMON.pokemon.map(poke => poke);
+console.log(showPokemon);
 
-/*window.data = {
-  showPokemon: function(POKEMON) {
-    for(let i=0; i<pokemon.lenght; i++){
-      return POKEMON.pokemon[0];
-    }  
-  }
-};
-console.log(showPokemon());*/
-
-const showPokemon = () => {
-  for(let i=0; i<pokemon.lenght; i++){
-  return POKEMON.pokemon[0];
-  }
-};
-
-window.data = {
-  showPokemon,
-};
+window.data = showPokemon;
