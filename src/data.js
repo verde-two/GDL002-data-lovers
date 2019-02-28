e8eada262f1a62fb18e3d5a9c14fb472ca5246f6
Didre <ixchel@hotmail.es>
2019-02-28 11:34:03 -0600

    probando copear y pegar

--- src/data.js
+++ src/data.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
@@ -31,6 +32,78 @@ window.data = {
     pokemon[i];
     }
     return POKEMON.pokemon[150];
+=======
+//Función para mostrar pokémon
+const showPokemon = POKEMON.pokemon.map(poke => poke);
+//console.log(showPokemon);
+
+/*const filterPoke = (data, pokeType) => {
+    return data.filter(pokemon => (pokemon.type == pokeType));
+  };
+//  console.log("este es filter Dixel", filterPoke(POKEMON.pokemon, 'Water'));
+*/
+const filterPoke = (data, type) => {
+  //console.log(data);
+    return data.filter((pokemon) => pokemon.type.includes(type));
+
+}
+//console.log("este es filter poke 2", filterPoke(POKEMON.pokemon, "Water"));*/
+/*productos.sort(function(a, b){
+	return a.nombre.localeCompare(b.nombre);
+});
+console.log(productos);
+*/
+const pokemonArray = Object.keys(POKEMON.pokemon).map(i => POKEMON.pokemon[i]);
+console.log(pokemonArray);
+const orderAZ = (data, order) =>{
+  return pokemonArray.sort((pokemon) => name.localeCompare(order));
+}
+console.log(orderAZ(POKEMON.pokemon, "name"));
+
+window.data = {
+    orderAZ,
+    showPokemon,
+    filterPoke
+};
+
+
+/*const filterPoke = function (data, type){
+  const newArray = [];
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[i].type.length; j++){
+      if (data[i].type[j] == type){
+        newArray.push(data[i]);
+      }
+    }
+>>>>>>> Stashed changes
   }
 }
+<<<<<<< Updated upstream
 console.log(data.showPokemon());
+=======
+console.log(filterPoke(POKEMON.pokemon, "Grass"));*/
+
+
+
+var productos = [
+ {id: 1, nombre: 'Refrigerador'},
+ {id: 8, nombre: 'Televisión'},
+ {id: 5, nombre: 'Estufa'},
+ {id: 7, nombre: 'Licuadora'},
+ {id: 3, nombre: 'Horno'}
+];
+// Ordenamiento por nombre, ascendente
+productos.sort(function(a, b){
+	return a.nombre.localeCompare(b.nombre);
+});
+console.log(productos);
+
+
+
+var palabras = ["uno", "dos", "tres"];
+var mayusculas = palabras.map(function (p) {
+ return p.toUpperCase();
+});
+
+console.log(mayusculas); // ["UNO", "DOS", "TRES"]
+>>>>>>> Stashed changes
--- src/main.js
+++ src/main.js
@@ -1,1 +1,61 @@
+<<<<<<< Updated upstream
 document.getElementById('root').innerHTML = data.showPokemon ();
+=======
+//Itera sobre objeto para mostrar a usuario cards específicas
+ const createCards = (data) => {
+    let cards = document.getElementById('cards');
+    while (cards.hasChildNodes()) {
+        cards.removeChild(cards.firstChild);
+    }
+    for(let i=0; i<data.length; i++) {
+      let card = document.createElement('DIV');
+      let parrNumb = document.createElement('P');
+      let parrName = document.createElement('P');
+      let parrType = document.createElement('P');
+      let image = document.createElement('IMG');
+      image.setAttribute('src', data[i].img);
+      let number= document.createTextNode(data[i].num);
+      let name = document.createTextNode(data[i].name);
+      let type = document.createTextNode(data[i].type);
+      parrNumb.appendChild(number);
+      parrName.appendChild(name);
+      parrType.appendChild(type);
+      card.appendChild(image);
+      card.appendChild(parrNumb);
+      card.appendChild(parrName);
+      card.appendChild(parrType);
+      card.setAttribute('class', 'card');
+      document.getElementById('cards').appendChild(card);
+  }
+};
+
+createCards(data.showPokemon);
+
+//Función obtiene valor de select para pasar argumento a filterPoke (función de filtrar)
+const addingFilter = () =>{
+  let select = document.getElementById('filterSelect');
+  let valueSelect = select.value;
+  let filterResult;
+  if (valueSelect == "All") {
+    filterResult = data.showPokemon;
+  } else {
+    filterResult = data.filterPoke(POKEMON.pokemon, valueSelect);
+}
+  createCards(filterResult);
+};
+
+document.getElementById('filterSelect').addEventListener("change", addingFilter);
+
+const addOrder = () =>{
+  let select = document.getElementById('orderAZ');
+  let valueSave = select.value;
+  let optResult;
+  if (valueSave == "pushOrder") {
+    optResult = data.addOrder;
+  } else {
+    optResult = data.orderAZ(POKEMON.pokemon,valueSave);
+  }
+  createCards(optResult);
+}
+document.getElementById('orderAZ').addEventListener("change", addOrder);
+>>>>>>> Stashed changes
